f819b7ada86e135f866ffab434a6bcfc70ba20c4
Israel Goncalves de Oliveira <prof.israel@gmail.com>
2019-01-05 20:44:18 -0200

    doc proj2

---
+++ proj2/load_results.py
@@ -0,0 +1,44 @@
+results = [['Air Cargo Problem 1', 'breadth_first_search', '', 20, 43, 56, 178, 6, 0.014622222999605583],\
+['Air Cargo Problem 1', 'depth_first_graph_search', '', 20, 21, 22, 84, 20, 0.004608410999935586],\
+['Air Cargo Problem 1', 'uniform_cost_search', '', 20, 60, 62, 240, 6, 0.012984271997993346],\
+['Air Cargo Problem 1', 'greedy_best_first_graph_search', ' with h_unmet_goals', 20, 7, 9, 29, 6, 0.0013301060025696643],\
+['Air Cargo Problem 1', 'greedy_best_first_graph_search', ' with h_pg_levelsum', 20, 6, 8, 28, 6, 0.36110015799931716],\
+['Air Cargo Problem 1', 'greedy_best_first_graph_search', ' with h_pg_maxlevel', 20, 6, 8, 24, 6, 0.12374628299949109],\
+['Air Cargo Problem 1', 'greedy_best_first_graph_search', ' with h_pg_setlevel', 20, 6, 8, 28, 6, 0.3611992830010422],\
+['Air Cargo Problem 1', 'astar_search', ' with h_unmet_goals', 20, 50, 52, 206, 6, 0.009081628002604702],\
+['Air Cargo Problem 1', 'astar_search', ' with h_pg_levelsum', 20, 28, 30, 122, 6, 0.1802382130008482],\
+['Air Cargo Problem 1', 'astar_search', ' with h_pg_maxlevel', 20, 43, 45, 180, 6, 0.13079704600022524],\
+['Air Cargo Problem 1', 'astar_search', ' with h_pg_setlevel', 20, 33, 35, 138, 6, 0.2942786610001349],\
+['Air Cargo Problem 2', 'breadth_first_search', '', 72, 3343, 4609, 30503, 9, 0.2626241800026037],\
+['Air Cargo Problem 2', 'depth_first_graph_search', '', 72, 624, 625, 5602, 619, 0.3713035319997289],\
+['Air Cargo Problem 2', 'uniform_cost_search', '', 72, 5154, 5156, 46618, 9, 0.4725405190001766],\
+['Air Cargo Problem 2', 'greedy_best_first_graph_search', ' with h_unmet_goals', 72, 17, 19, 170, 9, 0.020086606997210765],\
+['Air Cargo Problem 2', 'greedy_best_first_graph_search', ' with h_pg_levelsum', 72, 9, 11, 86, 9, 0.4624240320008539],\
+['Air Cargo Problem 2', 'greedy_best_first_graph_search', ' with h_pg_maxlevel', 72, 27, 29, 249, 9, 0.8930434720023186],\
+['Air Cargo Problem 2', 'greedy_best_first_graph_search', ' with h_pg_setlevel', 72, 9, 11, 84, 9, 0.952085534001526],\
+['Air Cargo Problem 2', 'astar_search', ' with h_unmet_goals', 72, 2467, 2469, 22522, 9, 0.4839449959981721],\
+['Air Cargo Problem 2', 'astar_search', ' with h_pg_levelsum', 72, 357, 359, 3426, 9, 11.6828805940022],\
+['Air Cargo Problem 2', 'astar_search', ' with h_pg_maxlevel', 72, 2887, 2889, 26594, 9, 68.58099545299774],\
+['Air Cargo Problem 2', 'astar_search', ' with h_pg_setlevel', 72, 1037, 1039, 9605, 9, 74.36665602699941],\
+['Air Cargo Problem 3', 'breadth_first_search', '', 88, 14663, 18098, 129625, 12, 0.5986446900024021],\
+['Air Cargo Problem 3', 'depth_first_graph_search', '', 88, 408, 409, 3364, 392, 0.1227982050004357],\
+['Air Cargo Problem 3', 'uniform_cost_search', '', 88, 18510, 18512, 161936, 12, 0.941394183999364],\
+['Air Cargo Problem 3', 'greedy_best_first_graph_search', ' with h_unmet_goals', 88, 25, 27, 230, 15, 0.00608379800178227],\
+['Air Cargo Problem 3', 'greedy_best_first_graph_search', ' with h_pg_levelsum', 88, 14, 16, 126, 14, 1.072023241998977],\
+['Air Cargo Problem 3', 'greedy_best_first_graph_search', ' with h_pg_maxlevel', 88, 21, 23, 195, 13, 1.187516988000425],\
+['Air Cargo Problem 3', 'greedy_best_first_graph_search', ' with h_pg_setlevel', 88, 35, 37, 345, 17, 4.498036878998391],\
+['Air Cargo Problem 3', 'astar_search', ' with h_unmet_goals', 88, 7388, 7390, 65711, 12, 0.6642969690001337],\
+['Air Cargo Problem 3', 'astar_search', ' with h_pg_levelsum', 88, 369, 371, 3403, 12, 18.008017989999644],\
+['Air Cargo Problem 3', 'astar_search', ' with h_pg_maxlevel', 88, 9580, 9582, 86312, 12, 328.8855454179975],\
+['Air Cargo Problem 3', 'astar_search', ' with h_pg_setlevel', 88, 3423, 3425, 31596, 12, 399.91184560200054],\
+['Air Cargo Problem 4', 'breadth_first_search', '', 104, 99736, 114953, 944130, 14, 4.3026164720031375],\
+['Air Cargo Problem 4', 'depth_first_graph_search', '', 104, 25174, 25175, 228849, 24132, 787.0532423440018],\
+['Air Cargo Problem 4', 'uniform_cost_search', '', 104, 113339, 113341, 1066413, 14, 6.708287541998288],\
+['Air Cargo Problem 4', 'greedy_best_first_graph_search', ' with h_unmet_goals', 104, 29, 31, 280, 18, 0.034335767002630746],\
+['Air Cargo Problem 4', 'greedy_best_first_graph_search', ' with h_pg_levelsum', 104, 17, 19, 165, 17, 1.7477340049990744],\
+['Air Cargo Problem 4', 'greedy_best_first_graph_search', ' with h_pg_maxlevel', 104, 56, 58, 580, 17, 4.247899269998015],\
+['Air Cargo Problem 4', 'greedy_best_first_graph_search', ' with h_pg_setlevel', 104, 107, 109, 1164, 23, 20.59132753800077],\
+['Air Cargo Problem 4', 'astar_search', ' with h_unmet_goals', 104, 34330, 34332, 328509, 14, 3.1022671290011203],\
+['Air Cargo Problem 4', 'astar_search', ' with h_pg_levelsum', 104, 1208, 1210, 12210, 15, 110.61845847600125],\
+['Air Cargo Problem 4', 'astar_search', ' with h_pg_maxlevel', 104, 62077, 62079, 599376, 14, 3222.123988618001],\
+['Air Cargo Problem 4', 'astar_search', ' with h_pg_setlevel', 104, 22606, 22608, 224229, 14, 4182.825753874]]
